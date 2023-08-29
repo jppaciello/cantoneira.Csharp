8a8089586d92847805aee12faa450b81867fd1bc
jppaciello <120607285+jppaciello@users.noreply.github.com>
2023-08-29 13:26:51 -0300

    Add files via upload

---
+++ Cantoneira.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantoneira
+{
+    public class Contar
+    {
+        public void ImprimirSequencia(int valor)
+        {
+            Console.WriteLine("cantoneira:");
+            int c = 0;
+            while(c <= valor)
+            {
+                Console.WriteLine(c);
+                c++;
+            }
+        }
+    }
+}
