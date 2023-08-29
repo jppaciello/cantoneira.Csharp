395b16abe60614acbe9ea1c37db948dc9bd0c49d
jppaciello <120607285+jppaciello@users.noreply.github.com>
2023-08-29 13:28:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Cantoneira;
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Informe um numero: ");
+        var v1 = Console.ReadLine();
+        int.TryParse(v1, out int valor);
+        Contar minhaInstancia = new Contar();
+        minhaInstancia.ImprimirSequencia(valor);
+    }
+}
+
